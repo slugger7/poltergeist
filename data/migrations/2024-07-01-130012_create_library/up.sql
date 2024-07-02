a2dbbf226d81c0c5eeb791e96d6de0e3bbdd98ee
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2024-07-02 07:41:18 +0200

    add unique constraints to library and library path

--- data/migrations/2024-07-01-130012_create_library/up.sql
+++ data/migrations/2024-07-01-130012_create_library/up.sql
@@ -1,6 +1,6 @@
 create table library (
   id serial primary key,
-  "name" varchar not null,
+  "name" varchar not null unique,
   created timestamp default current_timestamp not null,
   modified timestamp default current_timestamp not null
 )
--- data/migrations/2024-07-01-141316_create_library_path/up.sql
+++ data/migrations/2024-07-01-141316_create_library_path/up.sql
@@ -1,7 +1,7 @@
 create table library_path (
   id serial primary key,
   library_id int not null,
-  path varchar not null,
+  path varchar not null unique,
   created timestamp default current_timestamp not null,
   modified timestamp default current_timestamp not null,
   constraint fk_library
