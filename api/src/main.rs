ed110acaefdcf69c2904c80884ba4f25da2f4659
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2024-07-12 21:44:24 +0200

    use media functions to index videos

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,41 +1,66 @@
+use std::fs::DirEntry;
+
 use data::{
     establish_connection,
+    models::video::NewVideo,
     repositories::{
-        library_path_repository::create_library_path,
+        library_path_repository::{create_library_path, get_library_path_entity_by_id},
         library_repository::create_library,
-        video_repository::{create_video, get_videos_in_library_path, show_videos},
+        video_repository::{create_video, create_videos, get_videos_in_library_path, show_videos},
     },
 };
+use media::{create_relative_path, get_files_by_extensions_recursive};
 
 fn main() {
     let conn = &mut establish_connection();
-    let lib = create_library(conn, "Main Library unique 1");
-    let lib_path = create_library_path(conn, "some path again", &lib.id).unwrap();
-    create_video(
-        conn,
-        &lib_path.id,
-        "New video title",
-        "new file name",
-        &480,
-        &720,
-        &7000,
-        &2000,
-    );
-    create_video(
-        conn,
-        &lib_path.id,
-        "New video title number 2",
-        "new file name another file",
-        &480,
-        &720,
-        &7000,
-        &2000,
-    );
-    show_videos(conn);
-    let videos = get_videos_in_library_path(conn, lib_path);
-    println!("{} videos found", videos.len());
+    let lib = create_library(conn, "Default");
+    create_library_path(conn, "/media/sam/My Videos", &lib.id);
 
-    for vid in videos {
-        println!("Title: {}, File Name: {}", vid.title, vid.file_name)
+    if let Some(lib_path) = get_library_path_entity_by_id(conn, 1) {
+        println!("Fetching videos from disc");
+
+        let videos = get_files_by_extensions_recursive(
+            &lib_path.path,
+            &vec![
+                "mp4", "m4v", "mkv", "avi", "wmv", "flv", "webm", "f4v", "mpg", "m2ts", "mov",
+            ],
+        );
+
+        println!("Length {}", videos.len());
+
+        println!("Done fetching from disc\n");
+
+        println!("Adding videos to database");
+        let mut new_videos: Vec<NewVideo> = Vec::new();
+        for vid in videos {
+            match vid.file_name().into_string() {
+                Ok(filename) => {
+                    if let Some(absolute_path) = vid.path().to_str() {
+                        match create_relative_path(lib_path.path.clone(), absolute_path.to_string())
+                        {
+                            Ok(relative_path) => {
+                                let flnm: String = filename;
+                                new_videos.push(NewVideo {
+                                    library_path_id: &lib_path.id,
+                                    relative_path: relative_path,
+                                    file_name: flnm.clone(),
+                                    title: flnm.clone(),
+                                    height: &480,
+                                    width: &480,
+                                    runtime: &480,
+                                    size: &480,
+                                    checksum: None,
+                                })
+                            }
+                            Err(err) => print!("{}", err),
+                        }
+                    }
+                }
+                Err(_) => println!("Could not turn file name into a string"),
+            }
+        }
+
+        create_videos(conn, &new_videos);
     }
+    println!("Done");
 }
